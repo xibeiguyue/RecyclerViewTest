582fc005ab95e5fc0d33a434ca6135fa862d49de
Your Name <you@example.com>
2016-12-02 11:46:17 +0800

    添加HelloGit

---
+++ app/src/main/java/com/xibei/test/recyclerviewtest/HelloGit.java
@@ -0,0 +1,28 @@
+package com.xibei.test.recyclerviewtest;
+
+import android.content.SyncStatusObserver;
+import android.provider.Settings;
+
+/**
+ * Description:
+ * Author:Administrator
+ * Date: 2016/12/2
+ * Time: 11:41
+ */
+
+public class HelloGit {
+
+    private String name;
+
+    public HelloGit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
